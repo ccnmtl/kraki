d97b3a805bf88909f1d5dc2470acb5456a9466da
Anders Pearson <anders@columbia.edu>
2012-08-15 14:17:09 -0400

    basic push functionality working

--- kraki.py
+++ kraki.py
@@ -27,9 +27,51 @@ class RolfClient(object):
         url = ("%s/api/1.0/deployment/%d/push/"
                % (self.ROLF_BASE, int(deployment_id)))
         r = requests.post(url,
-                          params=dict(API_KEY=self.API_KEY),
+                          headers=dict(ROLF_API_KEY=self.API_KEY),
                           verify=False)
-        print r.status_code
+
+        if r.status_code != 200:
+            print r.status_code
+            print str(r.content)
+            return
+
+        for stage in r.json['stages']:
+            print "=== STAGE: %s" % stage['name']
+            sr = requests.post(
+                self.ROLF_BASE + stage['url'],
+                headers=dict(ROLF_API_KEY=self.API_KEY),
+                verify=False)
+            if sr.status_code != 200:
+                print "!!! Error: %d" % sr.status_code
+                print r.content
+                break
+            if sr.json['status'] != 'ok':
+                print "!!! Stage failed"
+                for l in sr.json['logs']:
+                    print "--- Command: "
+                    print indent(l['command'], ">>> ")
+                    if 'stdout' in l:
+                        print "--- STDOUT: "
+                        print indent(l['stdout'])
+                    if 'stderr' in l:
+                        print '--- STDERR: '
+                        print indent(l['stderr'])
+                break
+            print "Stage succeeded"
+            for l in sr.json['logs']:
+                if 'command' in l:
+                    print "--- Command: "
+                    print indent(l['command'], ">>> ")
+                if 'stdout' in l and l['stdout'] != u'':
+                    print "--- STDOUT: "
+                    print indent(l['stdout'])
+                if 'stderr' in l and l['stderr'] != u'':
+                    print '--- STDERR: '
+                    print indent(l['stderr'])
+
+
+def indent(t, prefix="    "):
+    return '\n'.join([prefix + l for l in t.splitlines()])
 
 
 if __name__ == "__main__":
