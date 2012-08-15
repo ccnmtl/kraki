de8add90c416b5144e9e373d25f80999fd77e331
Anders Pearson <anders@columbia.edu>
2012-08-15 14:58:26 -0400

    filling out setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="kraki",
+    version="0.1.0",
+    author="Anders Pearson",
+    author_email="anders@columbia.edu",
+    url="",
+    description="Commandline interface for Rolf",
+    long_description="Simple commandline tool for interfacing with the Rolf deployment manager",
+    install_requires = ['requests'],
+    scripts = [],
+    license = "BSD",
+    platforms = ["any"],
+    zip_safe=False,
+    package_data = {'' : ['*.*']},
+    packages=['kraki'],
+    test_suite='nose.collector',
+    )
